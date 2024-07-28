2eacf37e2b35fdf5d2743c803dd7e25d387cf5fb
Marcelo Santos <marcelo.santos@gseuclpjl4x25t4.lan>
2024-07-28 21:41:53 +0100

    New pages

--- js/main.js
+++ js/main.js
@@ -7,9 +7,7 @@
     responsive: true,
     parallaxBackgrounds: true,
     parallaxElements: true,
-    horizontalScrolling: false,
     hideDistantElements: false,
-    scrollProperty: 'scroll'
   });
 
 
